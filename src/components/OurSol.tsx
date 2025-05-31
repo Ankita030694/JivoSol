0c09787f2a44e3141c71b00f7d8596cc46eee682
Bhavy <bhavyajain817@gmail.com>
2025-05-31 16:46:51 +0530

    changes

--- src/components/OurSol.tsx
+++ src/components/OurSol.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Image from "next/image";
 
 export const OurSolution: FC = () => {
   const services = [
@@ -61,38 +62,48 @@ export const OurSolution: FC = () => {
 
       <div className="relative flex flex-col items-center space-y-8">
         {services.map((service, index) => (
-          <div key={index} className="group relative flex items-center w-full justify-center">
+          <div key={index} className="group relative flex items-center w-full">
             <div className="absolute w-screen h-[1px] left-1/2 -translate-x-1/2 flex justify-between pointer-events-none">
               <div className="w-0 h-full bg-[#055e31] group-hover:w-[25vw] transition-all duration-500 origin-right"></div>
               <div className="w-0 h-full bg-[#055e31] group-hover:w-[25vw] transition-all duration-500 origin-left"></div>
             </div>
-            <a
-              href={service.link}
-              className="flex items-center gap-6 transition-all duration-300"
-            >
-              <img
-                src={service.icon}
-                alt={service.name}
-                className="w-8 h-8 object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-              />
-              <div
-                className={`font-['Poppins',Helvetica] text-[40px] leading-[46.2px] whitespace-nowrap transition-all duration-300 ${
-                  service.highlighted || "group-hover:"
-                }font-semibold ${
-                    service.highlighted || "group-hover:"
-                  }text-black group-hover:text-[44px] group-hover:font-bold ${
-                  !service.highlighted && "font-light text-[#055e3142] hover:text-black"
-                }`}
-              >
-                {service.name}
-              </div>
-            </a>
             
             {/* Description box that appears on hover */}
-            <div className="absolute right-0 opacity-0 group-hover:opacity-100 transition-all duration-500 delay-300 transform translate-x-full -translate-y-1/2 top-1/2 pl-8">
-              <div className="bg-[#055e31] text-white p-4 rounded-lg shadow-lg w-[300px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-500">
+            <div className="absolute right-0 opacity-0 group-hover:opacity-100 transition-all duration-500 delay-300 transform translate-x-[calc(50%+1rem)] top-1/2 -translate-y-1/2">
+              <div className="text-black w-[300px] bg-white/90 p-4 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-500">
                 <p className="text-sm">{service.description}</p>
               </div>
+            </div>
+
+            <div className="flex items-center w-full mx-auto">
+              <div className="w-56 flex justify-start items-center">
+                <Image
+                  src={service.icon}
+                  alt={service.name}
+                  className="object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300" 
+                  width={48}
+                  height={48}
+                />
+              </div>
+              
+              <a
+                href={service.link}
+                className="flex-grow text-center transition-all duration-300"
+              >
+                <div
+                  className={`font-['Poppins',Helvetica] text-[40px] leading-[46.2px] whitespace-nowrap transition-all duration-300 ${
+                    service.highlighted || "group-hover:"
+                  }font-semibold ${
+                      service.highlighted || "group-hover:"
+                    }text-black group-hover:text-[44px] group-hover:font-bold ${
+                    !service.highlighted && "font-light text-[#055e3142] hover:text-black"
+                  }`}
+                >
+                  {service.name}
+                </div>
+              </a>
+              
+              <div className="w-24"></div> {/* Spacer for balance */}
             </div>
           </div>
         ))}
@@ -103,19 +114,6 @@ export const OurSolution: FC = () => {
           className="w-[472px] h-px object-cover"
           alt="Line"
           src="/line-1.svg"
-        />
-      </div>
-
-      <div className="absolute left-0 top-[376px] w-[373px] h-[314px]">
-        <img
-          className="w-[373px] top-20 left-0 absolute h-px object-cover"
-          alt="Line"
-          src="/line-2.svg"
-        />
-        <img
-          className="absolute w-[317px] h-[314px] top-0 left-7 object-cover"
-          alt="Rectangle"
-          src="/rectangle-1.svg"
         />
       </div>
     </section>
