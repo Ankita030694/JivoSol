d0b11d790359048565cf4ce48cf540115017e359
Bhavy <bhavyajain817@gmail.com>
2025-06-04 12:29:13 +0530

    changes

--- src/components/OurSol.tsx
+++ src/components/OurSol.tsx
@@ -60,61 +60,46 @@
           We help brands build and scale their digital presence through:
         </p>
 
-        <div className="relative flex flex-col items-center space-y-8">
+        <div className="relative flex flex-col items-center space-y-2">
           {services.map((service, index) => (
-            <div key={index} className="group relative flex items-center w-full">
-              <div className="absolute w-screen h-[1px] left-1/2 -translate-x-1/2 flex justify-between pointer-events-none">
-                <div className="w-0 h-full bg-[#055e31] group-hover:w-[25vw] transition-all duration-500 origin-right"></div>
-                <div className="w-0 h-full bg-[#055e31] group-hover:w-[25vw] transition-all duration-500 origin-left"></div>
-              </div>
-              
-              {/* Description box that appears on hover */}
-              <div className="absolute right-0 opacity-0 group-hover:opacity-100 transition-all duration-500 delay-300 transform translate-x-[calc(50%+1rem)] top-1/2 -translate-y-1/2">
-                <div className="text-black w-[300px] bg-white/90 p-4 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-500">
-                  <p className="text-sm">{service.description}</p>
-                </div>
-              </div>
-
-              <div className="flex items-center w-full mx-auto">
-                <div className="w-56 flex justify-start items-center">
+            <div key={index} className="relative w-full flex flex-col items-center group py-2">
+              {/* Row: Icon, Line, Title, Line */}
+              <div className="relative w-full flex items-center justify-center">
+                {/* Icon on the left, absolutely positioned, only visible on hover */} 
+                <div className="absolute left-0 top-1/2 -translate-y-1/2 z-10 opacity-0 group-hover:opacity-100 transition-all duration-500" style={{ transform: 'translateY(20%)' }}>
                   <Image
                     src={service.icon}
                     alt={service.name}
-                    className="object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300" 
-                    width={64}
-                    height={64}
+                    width={300}
+                    height={300}
                   />
                 </div>
-                
+                {/* Left line */}
+                <div className="flex-grow h-[2px] bg-[#055e31] transition-all duration-500 group-hover:mr-4 mr-0 opacity-0 group-hover:opacity-100" style={{ maxWidth: '100vw' }}></div>
+                {/* Service Name */}
                 <a
                   href={service.link}
-                  className="flex-grow text-center transition-all duration-300"
+                  className="block z-10 px-4 text-center flex-shrink-0"
                 >
                   <div
-                    className={`font-['Poppins',Helvetica] text-[40px] leading-[46.2px] whitespace-nowrap transition-all duration-300 ${
-                      service.highlighted || "group-hover:"
-                    }font-semibold ${
-                        service.highlighted || "group-hover:"
-                      }text-black group-hover:text-[44px] group-hover:font-bold ${
-                      !service.highlighted && "font-light text-[#055e3142] hover:text-black"
-                    }`}
+                    className={`font-['Poppins',Helvetica] text-[40px] leading-[46.2px] whitespace-nowrap transition-all duration-300 font-light text-[#055e3142] group-hover:text-black group-hover:font-bold group-hover:text-[44px]`}
                   >
                     {service.name}
                   </div>
                 </a>
-                
-                <div className="w-24"></div> {/* Spacer for balance */}
+                {/* Right line and Description above it */}
+                <div className="relative flex-grow flex flex-col items-end justify-center">
+                  <div className="h-[2px] bg-[#055e31] transition-all duration-500 group-hover:ml-4 ml-0 opacity-0 group-hover:opacity-100 w-full"></div>
+                  {/* Description above the right line, aligned right, never overflowing */}
+                  <div className="absolute -top-16 right-1/2 translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all duration-500 z-10 max-w-[90vw] w-[320px] flex justify-center">
+                    <p className="text-sm text-black p-4 text-center w-full">
+                      {service.description}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
-        </div>
-
-        <div className="absolute right-0 top-[456px]">
-          <img
-            className="w-[472px] h-px object-cover"
-            alt="Line"
-            src="/line-1.svg"
-          />
         </div>
       </section>
     );
