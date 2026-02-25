35ae3d1c3b1ff83be6b54ba2bb599e8538228766
Bhavy <bhavyajain817@gmail.com>
2026-02-25 17:06:17 +0530

    security: update next.js, react, and react-dom to patch CVE-2025-66478 and CVE-2025-67779

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['media-hosting.imagekit.io', 'images.unsplash.com', 'firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media-hosting.imagekit.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
   }
 };
 
